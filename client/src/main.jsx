8ada109a4ef46a2b0fe2dec6f2ac3cc38a6d9258
sanjay <sanjay2021cov@gmail.com>
2023-07-28 17:05:54 +0530

    auth config changed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-03ifqltxbr6nn0hn.us.auth0.com"
-      clientId="RXlGXkr49Ev5MHpvAC6vKkZ4bVn11iwl"
+      domain="dev-vsyu7g4e0078u34p.us.auth0.com"
+      clientId="SIQK2BEoURWSxK2Bqp71E6Bn462KAHzH"
       authorizationParams={{
         redirect_uri: "https://real-estate-virid.vercel.app",
       }}
