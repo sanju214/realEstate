d80b6302b4310f8ade9f9d47d684a469a8152d7c
sanjay <sanjay2021cov@gmail.com>
2023-07-28 17:48:25 +0530

    vhbh

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://real-estate-zeta-smoky.vercel.app/api",
+  baseURL: "https://real-estate-rho-six.vercel.app//api",
 });
 
 export const getAllProperties = async () => {
