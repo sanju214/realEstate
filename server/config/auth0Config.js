3338d0da64287c7da8124340c5338ef53de0fec4
sanjay <sanjay2021cov@gmail.com>
2023-08-07 18:29:53 +0530

    autho0 audience changed to ip

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-  audience: "http://localhost:8000",
+  audience: "http://10.5.41.22:8000",
   issuerBaseURL: "https://dev-vsyu7g4e0078u34p.us.auth0.com",
   tokenSigningAlg: "RS256",
 });
